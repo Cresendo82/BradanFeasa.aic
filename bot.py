f9849a6ccc8def2b438437283e147774e86c4171
Cresendo <56112657+Cresendo-dev@users.noreply.github.com>
2020-05-28 07:53:15 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(scp):
+    if scp.author == client.user:
+        return
+
+    if scp.content.startswith('!ping'):
+
+        await scp.channel.send('pong')
+
+    if scp.content.startswith('!scp'):
+        
+        try:
+            info = scp.content.split(" ")[1]
+            region = scp.content.split(" ")[2]
+            lfo = len(info)
+
+            if lfo == 1:
+                embed=discord.Embed(title= f"http://ko.scp-wiki.net/scp-00" + info + "-" + region, description=f"", color=0x23bb76)
+                await scp.channel.send(embed=embed)
+
+            if lfo == 2:
+                embed=discord.Embed(title= f"http://ko.scp-wiki.net/scp-0" + info + "-" + region, description=f"", color=0x23bb76)
+                await scp.channel.send(embed=embed)
+
+            if lfo == 3 or lfo == 4:
+                embed=discord.Embed(title= f"http://ko.scp-wiki.net/scp-" + info + "-" + region, description=f"", color=0x23bb76)
+                await scp.channel.send(embed=embed)
+
+        except IndexError:
+            embed=discord.Embed(title= f"뒤에 검색할 내용을 적어주세요.", description=f"예를 들어, !scp 300 ko 이렇게 말이죠.", color=0xf3bb76)
+            await scp.channel.send(embed=embed)
+
+client.run('NzEzNjkwODQ4Nzg0NTQ3OTMw.Xs4-qg.H02l_yzpGqX7sqhsSiZyahnJx0c')
