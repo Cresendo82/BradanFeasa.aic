77e81cdd7b8d6c65064a9daeee0c8a4825a59852
Cresendo <56112657+Cresendo-dev@users.noreply.github.com>
2020-05-29 08:31:13 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,10 +22,10 @@ async def on_message(scp):
         if i == 1:
             await scp.channel.send('지혜의 연어, 브라단입니다.')
             
-        if i == 1:
+        if i == 2:
             await scp.channel.send('우리들의 동무, 브라단입니다.')
             
-        if i == 1:
+        if i == 3:
             await scp.channel.send('뭐든지 물어봐주세요, 브라단입니다.')            
                         
     if scp.content.startswith('!구글'):
