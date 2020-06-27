e2b6a4f84962f65311efb56ecc7cbff59d68cc4c
Cresendo <56112657+Cresendo-dev@users.noreply.github.com>
2020-06-27 20:01:00 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ async def on_message(scp):
         
         embed=discord.Embed(title= f"검색 결과", description=f"", color=0x23bb76)
         embed.add_field(name="'" + info + "'" + " 유저 검색 결과", value='[{0}](<{1}>)'.format(info, link), inline=False)
-        embed.add_field(name="'" + soup.select("#user-info-area > dl > dt:nth-child(1) > a") + "'" + "가입된 위키닷 페이지", value='', inline=False)
+        embed.add_field(name=soup.select("#user-info-area > dl > dt:nth-child(1) > a"), value='', inline=False)
         await scp.channel.send(embed=embed)
         
     if scp.content.startswith('!태그'):
