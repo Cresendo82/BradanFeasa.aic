8fd4e8f0fb51d6ab49b4f5e5aa31c065c46ad4b0
Cresendo <56112657+Cresendo-dev@users.noreply.github.com>
2020-06-28 16:45:03 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,11 +84,11 @@ async def on_message(scp):
         req = requests.get(link)
         html = req.text
         soup = BeautifulSoup(html, 'html.parser')
-        my = soup.find_all(attrs={'herf':'http://ko.scp-wiki.net'})    
+        my = soup.select('#user-info-area > dl > dt > a')
         
         embed=discord.Embed(title= f"검색 결과", description=f"", color=0x23bb76)
         embed.add_field(name="'" + info + "'" + " 유저 검색 결과", value='[{0}](<{1}>)'.format(info, link), inline=False)   
-        embed.add_field(name='유저 가입 현황', value=my, inline=False)
+        embed.add_field(name='유저 가입 현황', value='{0}'.format(my), inline=False)
             
         await scp.channel.send(embed=embed)
         
