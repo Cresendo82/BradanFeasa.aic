cbf0f740d8bd0e828600fea9f2f95ded24a2ce34
Cresendo <56112657+Cresendo82@users.noreply.github.com>
2020-11-01 12:49:16 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import feedparser
 import requests
 from bs4 import BeautifulSoup
 
-client = discord.Client()
+client = discord.Client(guild_subscriptions(true))
 
 @client.event
 async def on_ready():
